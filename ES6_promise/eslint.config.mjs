ad31be7e2b1b89cf08038a0924dbadea2ae2e45b
Huang1810 <ahhamed1810@gmail.com>
2024-07-28 00:11:14 +0100

    commit

---
+++ ES6_promise/eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        setTimeout: 'readonly',
+        require: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        module: 'readonly',
+      },
+    },
+    rules: {
+      // Your ESLint rules here
+    },
+  },
+];
