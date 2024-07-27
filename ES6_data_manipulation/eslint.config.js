9681f2d0ead7927e4741ebf7554221c1796189ee
Huang1810 <ahhamed1810@gmail.com>
2024-07-27 23:35:53 +0100

    commit

--- ES6_data_manipulation/eslint.config.js
+++ ES6_data_manipulation/eslint.config.js
@@ -1,14 +1,21 @@
-module.exports = {
-  env: {
-    es2021: true,
-    node: true,
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        require: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        module: 'readonly',
+      },
+    },
+    rules: {
+      // Your ESLint rules here
+    },
   },
-  extends: 'eslint:recommended',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  rules: {
-    // Your ESLint rules
-  },
-};
+];
