761b48fa216e3945b854376f626762f2ad5de0aa
Huang1810 <ahhamed1810@gmail.com>
2024-07-28 01:17:21 +0100

    commit

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,10 @@
 export default function handleResponseFromAPI(promise) {
     return promise
       .then(() => {
-        console.log('Got a response from the API');
-        return { status: 200, body: 'success' };
+        return { status: 200, body: 'success', message: 'Got a response from the API' };
       })
       .catch(() => {
-        console.log('Got a response from the API');
-        return new Error();
+        return { status: 'error', message: 'Got a response from the API' };
       });
   }
+  
