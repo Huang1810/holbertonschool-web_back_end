d292d3baccb499419eb7c598c4b4d2be45d06a20
Huang1810 <ahhamed1810@gmail.com>
2024-08-24 23:10:50 +0100

    commit

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+def index_range(page, page_size):
+    """result"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
